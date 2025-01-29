adf7a32eea67e93d92d1deb6b8922f311d7e0a42
Евгений Толкач <tolkachevgeniy007@gmail.com>
2025-01-29 23:38:00 +0300

    доработали файл index_js

--- lesson_1_3/index.js
+++ lesson_1_3/index.js
@@ -5,7 +5,11 @@ const item = prompt("Введите наименование товара");
 const count = +prompt("Введите количество товара");
 const category = prompt("Введите категорию товара");
 const price = +prompt("Введите цену за единицу товара");
-
-console.log(`На складе ${count} ${item} на общую сумму ${count * price}`);
+if (count >= 1 && price >= 1 || count === 0 && price === 0) {
+   console.log (`На складе ${count} ${item} на сумму ${count * price}`)
 }
+else (
+   console.log (`Вы ввели некорректные данные`)
+)
+};
 
