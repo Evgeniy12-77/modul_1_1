4eeeb46cdaf90e10fc3fd724b3e0594fce571a76
Евгений Толкач <tolkachevgeniy007@gmail.com>
2025-02-01 15:29:29 +0300

    сделали 1.6 task_1 и task_2

---
+++ Lesson_1_6/task_01.js
@@ -0,0 +1,11 @@
+'use strict'
+
+for (let a = 2; a < 10; a++) {
+   console.log('-------------');
+   for (let b = 2; b < 10; b++) {
+      console.log (`${a} ** ${b} = ${a ** b}`);
+   };
+};
+
+
+
