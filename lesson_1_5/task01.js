b13945fbc4be67be5998be6996f23f057805a0a3
Евгений Толкач <tolkachevgeniy007@gmail.com>
2025-02-01 14:31:40 +0300

    изменили task

--- lesson_1_5/task01.js
+++ lesson_1_5/task01.js
@@ -25,11 +25,10 @@ const calculate = (sum, items, promo) => {
    let copiSum = sum;
    let copiItems = items;
    let copiPromo = promo;
-   
-   const calculateItems = () => {
+  const calculateItems = () => {
       if (copiItems >= 10) {
          return 0.03 * copiSum;
-      } else  0;
+      } else return 0;
    };
 
    const res_1 = calculateItems();
@@ -52,22 +51,13 @@ const calculate = (sum, items, promo) => {
       } else return 0;
    };
    const res_4 = calculatePromo_2 ();
-
    const res_5 = res_1 + res_2 + res_3 + res_4;
-
    const sumTotal = copiSum - res_5;
-   return  `Ваша корзина товаров с учетом всех скидок равна ${sumTotal} рублей` ;
-};    
-
-   
-
-
-    
-
+   return  `Ваша корзина товаров с учетом всех скидок равна ${sumTotal} рублей`;    
+};     
 console.log (calculate(40000, 10, 'METHED'));
-console.log (calculate(40000, 10, 'G3H2Z1'));
-console.log (calculate(40000, 10,));
-console.log (calculate('hkfd', 'kdfwl'))
+console.log (calculate(40000, 5, 'G3H2Z1'));
+console.log (calculate(10, 10));
 
 
 
