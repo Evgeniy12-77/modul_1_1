8111b6462c511cb26061b667f89cf254fe1c3529
Евгений Толкач <tolkachevgeniy007@gmail.com>
2025-02-12 12:42:57 +0300

    установили eslint и отредактировали cart_1.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-'use strict'
+'use strict';
 {
-   const item = 'smart проектор philips';
-   const count = 15;
-   const category = 'видео техника';
-   const price = 1200;
- 
-   console.log(item);
-   console.log(`общая сумма товара: ${count * price}`);
- }
- 
- {
-   const item = 'смартфон Xiaomi Redmi 14';
-   const count = 25;
-   const category = 'мобильные телефоны и электроника';
-   const price = 1000;
- 
-   console.log(item);
-   console.log(`общая сумма товара: ${count * price}`);
- }
+  const item = 'smart проектор philips';
+  const count = 15;
+  //const category = 'видео техника';
+  const price = 1200;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+  const item = 'смартфон Xiaomi Redmi 14';
+  const count = 25;
+  //const category = 'мобильные телефоны и электроника';
+  const price = 1000;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
--- lesson_1_10/cart_1.js
+++ lesson_1_10/cart_1.js
@@ -1,49 +1,48 @@
 const cart = {
-   items: [],
-   totalPrice: 0,
-   count: 0,
-   
-   // Геттер для totalPrice
-   get totalPrice() {
-       return this.calculateItemPrice();
-   },
-   
-   calculateItemPrice() {
-       // Возвращает общую стоимость без изменения состояния
-       return this.items.reduce((acc, item) => acc + item.price * item.quantity, 0);
-   },
-   
-   increaseCount(amount) {
-       this.count += amount; 
-   },
-   
-   add(name, price, quantity = 0) {
-       const item = {
-           name: name,
-           price: price,
-           quantity: quantity
-       };
-       this.items.push(item);
-       this.increaseCount(quantity); // Увеличивает count через метод increaseCount
-       // Вызываем метод calculateItemPrice при необходимости, но не сохраняем в totalPrice
-   },
-   
-   clear() {
-       this.items = [];
-       this.count = 0;
-   },
-   
-   print() {
-       console.log(JSON.stringify(this.items, null, 2));
-       console.log(`Общая стоимость корзины: ${this.totalPrice}`); // Используем геттер для получения стоимости
-   }
-};
+items: [],
+  totalPrice: 0,
+  count: 0,
 
+  // Геттер для totalPrice
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
 
-// Пример добавления товаров в корзину
-cart.add("Товар 1", 100, 2);
-cart.add("Товар 2", 200, 1);
-cart.add("Товар 3", 50, 5);
+  calculateItemPrice() {
+    // Возвращает общую стоимость без изменения состояния
+    return this.items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  },
+
+  increaseCount(amount) {
+    this.count += amount;
+  },
+
+  add(name, price, quantity = 0) {
+    const item = {
+      name,
+      price,
+      quantity,
+    };
+    this.items.push(item);
+    this.increaseCount(quantity); // Увеличивает count через метод increaseCount
+    // Вызываем метод calculateItemPrice при необходимости, но не сохраняем в totalPrice
+  },
+
+  clear() {
+    this.items = [];
+    this.count = 0;
+  },
+
+  print() {
+    console.log(JSON.stringify(this.items, null, 2));
+   console.log(`Общая стоимость корзины: ${this.totalPrice}`); 
+   // Используем геттер для получения стоимости
+  },
+},
+
+cart.add('Товар 1', 100, 2);
+cart.add('Товар 2', 200, 1);
+cart.add('Товар 3', 50, 5);
 
 // Вызов метода print для вывода информации в консоль
 cart.print();
