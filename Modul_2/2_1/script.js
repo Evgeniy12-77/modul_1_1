e93321d338d36d73dd77a9969ea47905d0ac5444
Евгений Толкач <tolkachevgeniy007@gmail.com>
2025-03-08 13:08:33 +0300

    доделали 2.1

--- Modul_2/2_1/script.js
+++ Modul_2/2_1/script.js
@@ -5,11 +5,15 @@ const lists = document.querySelectorAll('.props__list');
 const itemsFour = document.querySelectorAll('.props__item_four');
 const ads = document.querySelector('.ads');
 const itemsTwo = document.querySelectorAll('.props__item_two');
+const itemsTitles = document.querySelectorAll('.item__title');
+
 
 console.log(lists);
 console.log(items);
 console.log(itemsFour);
 console.log(itemsTwo);
+console.log(itemsTitles);
+
 
 items[0].replaceWith(items[1]);
 items[3].after(items[0]);
@@ -22,4 +26,10 @@ itemsTwo[7].remove();
 itemsTwo[8].remove();
 itemsTwo[6].append(itemsTwo[7]);
 itemsTwo[7].append(itemsTwo[8]);
+itemsTitles[1].remove();
+itemsTitles[3].remove();
+itemsTitles[4].remove();
+lists[2].before(itemsTitles[3]);
+lists[3].before(itemsTitles[4]);
+lists[5].before(itemsTitles[1]);
 
